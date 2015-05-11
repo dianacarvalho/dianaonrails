e1fbe858da17704deffdbc897552b81622b45d6a
diana <diana.carvalho@tutanota.com>
2015-05-11 22:51:17 +0100

    database

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'dianaonrails-cape8g7pab.elasticbeanstalk.com' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
